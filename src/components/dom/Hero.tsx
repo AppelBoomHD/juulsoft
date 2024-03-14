5c87887c18d0b8182b58fba3b897ff516715a17f
Julian Riemersma <julian@alserdamedia.nl>
2024-03-14 15:51:33 +0100

    feat(hero): add typewriter effect

--- src/components/dom/Hero.tsx
+++ src/components/dom/Hero.tsx
@@ -1,6 +1,7 @@
 import { useRef } from 'react';
 import { useScroll } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
+import Typewriter from 'typewriter-effect';
 
 import Mouse from './Mouse';
 
@@ -14,8 +15,16 @@ export default function Hero() {
 
   return (
     <section ref={ref} className="h-full">
-      <p className="text-md absolute left-1/2 top-3/4 -translate-x-1/2 text-center font-bold sm:text-2xl">
-        Hi, I&apos;m Julian Riemersma, Fullstack developer
+      <p className="absolute left-1/2 top-3/4 -translate-x-1/2 text-center font-nunito text-lg font-bold sm:text-3xl">
+        <Typewriter
+          options={{}}
+          onInit={(typewriter) => {
+            typewriter
+              .changeDelay(50)
+              .typeString(`Hi, I'm Julian Riemersma, Fullstack developer`)
+              .start();
+          }}
+        />
       </p>
       <Mouse />
     </section>
