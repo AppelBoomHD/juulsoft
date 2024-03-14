390afc124ad3faa560651bf51994d55ddae70f94
Julian Riemersma <julian@alserdamedia.nl>
2024-03-14 15:38:26 +0100

    feat(logo): add resume easter egg

--- src/components/dom/Header.tsx
+++ src/components/dom/Header.tsx
@@ -2,14 +2,24 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header className="absolute left-12 top-12 z-10">
-      <Image
-        src="/logo.svg"
-        alt="logo"
-        width={48}
-        height={48}
-        className="transition-transform hover:-rotate-90 hover:cursor-pointer"
-      />
+    <header>
+      <a
+        title="press to view my resume!"
+        href="https://docs.google.com/document/d/1TrOtrdLJ3ULGnvKA8jTK5MpcFYYfH-lz/edit?usp=sharing&ouid=102620587897862864426&rtpof=true&sd=true"
+        target="_blank"
+        className="group absolute left-12 top-12 z-10 flex items-end hover:animate-pulse hover:cursor-pointer"
+      >
+        <Image
+          src="/logo.svg"
+          alt="logo"
+          width={48}
+          height={48}
+          className="transition-transform group-hover:-rotate-180"
+        />
+        <div className="invisible -z-10 flex -translate-x-5 font-nunito text-4xl font-bold tracking-wide text-lightPurple transition-all group-hover:visible group-hover:translate-x-1">
+          esume
+        </div>
+      </a>
     </header>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,7 @@ module.exports = {
       },
       fontFamily: {
         sans: ['Geomanist', 'sans-serif'],
+        nunito: ['NunitoSans'],
       },
     },
   },
